98dcbfdd5177262279081c208d3dba030f6455c6
Devika88832211111 <140135963+Devika88832211111@users.noreply.github.com>
2023-07-22 00:44:15 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,54 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(row[col] == player for row in board):
+            return True
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+def is_board_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    player_index = 0
+
+    while True:
+        print_board(board)
+
+        player = players[player_index]
+        print(f"Player {player}'s turn.")
+        row = int(input("Enter the row (0-2): "))
+        col = int(input("Enter the column (0-2): "))
+
+        if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == " ":
+            board[row][col] = player
+
+            if check_winner(board, player):
+                print_board(board)
+                print(f"Player {player} wins!")
+                break
+
+            if is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+
+            player_index = 1 - player_index
+        else:
+            print("Invalid move. Try again.")
+
+if __name__ == "__main__":
+    tic_tac_toe()
